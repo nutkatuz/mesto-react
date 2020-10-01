226a8d4d956160e608e9ea16849642f50d361181
Anna Revidovich <nutkatuz@yandex.ru>
2020-10-01 11:30:08 +0500

    Updates

--- README.md
+++
@@ -1,6 +0,0 @@
-### Проект Mesto - приложение на React
-
-Запустить приложение в браузере [http://localhost:3000](http://localhost:3000)  с помощью команды
- `npm start`
-
-Планируется выложить на GHР ([deployment](https://facebook.github.io/create-react-app/docs/deployment)) как только я допишу все фичи
---
+++ precache-manifest.094f5871ae0471582f5237972d138cfc.js
@@ -0,0 +1,106 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "58bd35a03a3cdc39914333b6b8cf6f51",
+    "url": "/mesto-react/index.html"
+  },
+  {
+    "revision": "c06a4b2ec6b301fb76d5",
+    "url": "/mesto-react/static/css/main.b1aca8f3.chunk.css"
+  },
+  {
+    "revision": "e7086ba4e17babb670b9",
+    "url": "/mesto-react/static/js/2.381772aa.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/mesto-react/static/js/2.381772aa.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "c06a4b2ec6b301fb76d5",
+    "url": "/mesto-react/static/js/main.1bf0840f.chunk.js"
+  },
+  {
+    "revision": "aa6c10004297a7415d3d",
+    "url": "/mesto-react/static/js/runtime-main.1a010d5a.js"
+  },
+  {
+    "revision": "438d040613f4ab457a2bd158608f9068",
+    "url": "/mesto-react/static/media/Inter-Black.438d0406.woff"
+  },
+  {
+    "revision": "bde42e677b54fc0d2f83aa74d63e77e4",
+    "url": "/mesto-react/static/media/Inter-Black.bde42e67.otf"
+  },
+  {
+    "revision": "c9070b12f65be5198ca4dcdca7cf4266",
+    "url": "/mesto-react/static/media/Inter-Black.c9070b12.ttf"
+  },
+  {
+    "revision": "d2f835f3c9535d55d27e63f4b3da5cfb",
+    "url": "/mesto-react/static/media/Inter-Black.d2f835f3.woff2"
+  },
+  {
+    "revision": "0ed54f43055ff8b96c54dd4a8c30decf",
+    "url": "/mesto-react/static/media/Inter-Bold.0ed54f43.woff"
+  },
+  {
+    "revision": "53dcdee153bfdb439455242d611e7afd",
+    "url": "/mesto-react/static/media/Inter-Bold.53dcdee1.woff2"
+  },
+  {
+    "revision": "753bd86618b0faefb54da21403a261bb",
+    "url": "/mesto-react/static/media/Inter-Bold.753bd866.otf"
+  },
+  {
+    "revision": "e91fa64e257efa91c890c6dcb42a6334",
+    "url": "/mesto-react/static/media/Inter-Bold.e91fa64e.ttf"
+  },
+  {
+    "revision": "58b22f90caa86e59747ab7020e890857",
+    "url": "/mesto-react/static/media/Inter-Regular.58b22f90.woff2"
+  },
+  {
+    "revision": "9a9eeddb3eb9ce4f64e378a8a7b9c042",
+    "url": "/mesto-react/static/media/Inter-Regular.9a9eeddb.otf"
+  },
+  {
+    "revision": "a5e1eecae07839e1412d26ecac573fe1",
+    "url": "/mesto-react/static/media/Inter-Regular.a5e1eeca.woff"
+  },
+  {
+    "revision": "c706da9a8ae9dca2e5349410bd6c4952",
+    "url": "/mesto-react/static/media/Inter-Regular.c706da9a.ttf"
+  },
+  {
+    "revision": "d476a8b86d0b656cfa463c1148e96069",
+    "url": "/mesto-react/static/media/avatarbtn.d476a8b8.svg"
+  },
+  {
+    "revision": "c2d0901f14de58acae456bda23f80e8a",
+    "url": "/mesto-react/static/media/close.c2d0901f.svg"
+  },
+  {
+    "revision": "c8252db2902c59bff2cb619de2b75c2f",
+    "url": "/mesto-react/static/media/editbutton.c8252db2.svg"
+  },
+  {
+    "revision": "a33fcd100d0d943b7cd693be0237d16c",
+    "url": "/mesto-react/static/media/heart.a33fcd10.svg"
+  },
+  {
+    "revision": "9fdebb6b009c16af50fae58657e30a94",
+    "url": "/mesto-react/static/media/heart_black.9fdebb6b.svg"
+  },
+  {
+    "revision": "855a8c9865e79463f31e74f9abcb4438",
+    "url": "/mesto-react/static/media/logo.855a8c98.svg"
+  },
+  {
+    "revision": "bdfb8ebb113b9312e080c2f4b4213223",
+    "url": "/mesto-react/static/media/plus.bdfb8ebb.svg"
+  },
+  {
+    "revision": "efa6fb04512d4ea565aaa4109835b46d",
+    "url": "/mesto-react/static/media/trash.efa6fb04.svg"
+  }
+]);
